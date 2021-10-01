1be6b8c74bba7d966c7f3ae174a5cdb21edd7328
Gustavo Scabuzzi Gonçalves <34173963+GustavoScabuzzi@users.noreply.github.com>
2021-10-01 20:21:40 -0300

    [Feature Gustavo - Task: Testando Gitignore]

--- src/main/java/com/generation/farmacia/repositories/CategoriaRepository.java
+++ src/main/java/com/generation/farmacia/repositories/CategoriaRepository.java
@@ -1,20 +1,5 @@
 package com.generation.farmacia.repositories;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.generation.farmacia.models.Categoria;
-
-@Repository
-public class CategoriaRepository extends JpaRepository <Categoria, Long>{
-		
-	/**
-	 *@author George
-	 *@since 1.0
-	 */
-	public List<Categoria> findAllByDescricaoCategoriaContainingIgnoreCase(String descricaoCategoria);
-
+public interface CategoriaRepository {
 
 }
