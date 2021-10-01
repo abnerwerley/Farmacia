ab9e479d035ce7d589bae5ccdb72915cbb61d440
George <george_roots@hotmail.com>
2021-10-01 18:29:37 -0300

    [Feature George - Task ADD extends JPA]

--- src/main/java/com/generation/farmacia/repositories/CategoriaRepository.java
+++ src/main/java/com/generation/farmacia/repositories/CategoriaRepository.java
@@ -5,16 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.generation.farmacia.models.Categoria;
 
 @Repository
-public class CategoriaRepository extends JpaRepository <categoria, Long>{
+public class CategoriaRepository extends JpaRepository <Categoria, Long>{
 		
-	/**
-	 * @return Lista com todos as descriçoes que contenham a palavra digitada
-	 * @author George
-	 * @since 1.0
-	 */
-	public List<Categoria> findAllByDescricaoCategoriaContainingIgnoreCase(String descricaoCategoria);
-
+	
 
 }
