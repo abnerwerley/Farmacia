b6435159c3f9bbb6b656315beeb1349f17b6b4ff
George <george_roots@hotmail.com>
2021-10-01 18:26:54 -0300

    [Feature George - Task ADD class CategoriaRepository]

--- src/main/java/com/generation/farmacia/repositories/CategoriaRepository.java
+++ src/main/java/com/generation/farmacia/repositories/CategoriaRepository.java
@@ -1,5 +1,20 @@
 package com.generation.farmacia.repositories;
 
-public class CategoriaRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class CategoriaRepository extends JpaRepository <categoria, Long>{
+		
+	/**
+	 * @return Lista com todos as descriçoes que contenham a palavra digitada
+	 * @author George
+	 * @since 1.0
+	 */
+	public List<Categoria> findAllByDescricaoCategoriaContainingIgnoreCase(String descricaoCategoria);
+
 
 }
